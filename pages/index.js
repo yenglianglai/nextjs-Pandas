8dfe082e911d2da78e1875775cd328b913a3148a
chunkuei <ryanlai880122@gmail.com>
2022-07-26 18:04:17 +0800

    homepage edit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head"
 import Image from "next/image"
 import Header from "../components/header"
+import Welcome from "../components/welcome"
 import { useMoralis } from "react-moralis"
 import pinguEatGif from "../public/nom-nom-pingu.gif"
 import pinguDeliverGif from "../public/pingu-deliver.gif"
 import Link from "next/link"
+import { BannerStrip } from "web3uikit"
 
 const supportedChains = ["31337", "4"] // hardhat, rinkeby
 
@@ -14,37 +16,42 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Pandas</title>
+                <title>Penguine</title>
                 <meta name="description" content="A decentralized food delivery service" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main>
                 <Header></Header>
+                <Welcome></Welcome>
                 {isWeb3Enabled ? (
                     supportedChains.includes(parseInt(chainId).toString()) ? (
-                        <div className="flex flex-row p-15 mt-10 place-content-around place-content-center">
+                        <div className="flex flex-row my-20 place-content-around place-content-center">
                             <Link href="/order">
                                 <div className="basis-1/3 entranceButton rounded-lg overflow-hidden">
-                                    <Image src={pinguEatGif} height={450} width={400}></Image>
-
                                     <div className="entranceBanner">I'm Hungryyyy!</div>
+                                    <Image src={pinguEatGif} height={450} width={420}></Image>
                                 </div>
                             </Link>
                             <Link href="/deliver">
                                 <div className="basis-1/3 entranceButton rounded-lg overflow-hidden">
-                                    <Image src={pinguDeliverGif} height={450} width={400}></Image>
                                     <div className="entranceBanner">I'm ready to send Food!</div>
+                                    <Image src={pinguDeliverGif} height={450} width={420}></Image>
                                 </div>
                             </Link>
                         </div>
                     ) : (
-                        <div className="font-normal">
-                            {`Please switch to the supported chain ID, the supported chain IDs are: ${supportedChains}.`}
+                        <div>
+                            <BannerStrip
+                                text={`Please switch to the supported chain ID, the supported chain IDs are: ${supportedChains}.`}
+                                type="warning"
+                            />
                         </div>
                     )
                 ) : (
-                    <div>Please connect to a Wallet</div>
+                    <div>
+                        <BannerStrip text="Please connect to a wallet." type="error" />
+                    </div>
                 )}
             </main>
         </div>
