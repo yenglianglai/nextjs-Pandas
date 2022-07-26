852482b4866b371aaabe355b2953e983b1b9fd21
chunkuei <ryanlai880122@gmail.com>
2022-07-26 16:27:11 +0800

    something new

--- components/header.js
+++ components/header.js
@@ -1,9 +1,12 @@
 import { ConnectButton } from "web3uikit"
+import Image from "next/image"
+import IconPNG from "../public/icon.png"
 
 export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 font-semibold text-2xl md:text-3xl"> Feed the Penguine</h1>
+            {/* <h1 className="py-4 px-4 font-semibold text-2xl md:text-3xl"> Feed the Penguine</h1> */}
+            <Image src={IconPNG} height={25} width={25}></Image>
             <div className="ml-auto py-2 px-4" id="connectButton">
                 <ConnectButton moralisAuth={false} size="small" />
             </div>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from "../components/header"
 import { useMoralis } from "react-moralis"
 import pinguEatGif from "../public/nom-nom-pingu.gif"
 import pinguDeliverGif from "../public/pingu-deliver.gif"
+import Link from "next/link"
 
 const supportedChains = ["31337", "4"] // hardhat, rinkeby
 
@@ -23,31 +24,19 @@ export default function Home() {
                 {isWeb3Enabled ? (
                     supportedChains.includes(parseInt(chainId).toString()) ? (
                         <div className="flex flex-row p-15 mt-10 place-content-around place-content-center">
-                            <a
-                                className="basis-1/3 entranceButton rounded-lg overflow-hidden"
-                                href="/order"
-                            >
-                                <Image
-                                    className="object-fill"
-                                    src={pinguEatGif}
-                                    height={450}
-                                    width={400}
-                                ></Image>
+                            <Link href="/order">
+                                <div className="basis-1/3 entranceButton rounded-lg overflow-hidden">
+                                    <Image src={pinguEatGif} height={450} width={400}></Image>
 
-                                <div className="entranceBanner">I'm Hungryyyy!</div>
-                            </a>
-                            <a
-                                className="basis-1/3 entranceButton rounded-lg overflow-hidden"
-                                href="/deliver"
-                            >
-                                <Image
-                                    className="object-fill"
-                                    src={pinguDeliverGif}
-                                    height={450}
-                                    width={400}
-                                ></Image>
-                                <div className="entranceBanner">I'm ready to send Food!</div>
-                            </a>
+                                    <div className="entranceBanner">I'm Hungryyyy!</div>
+                                </div>
+                            </Link>
+                            <Link href="/deliver">
+                                <div className="basis-1/3 entranceButton rounded-lg overflow-hidden">
+                                    <Image src={pinguDeliverGif} height={450} width={400}></Image>
+                                    <div className="entranceBanner">I'm ready to send Food!</div>
+                                </div>
+                            </Link>
                         </div>
                     ) : (
                         <div className="font-normal">
